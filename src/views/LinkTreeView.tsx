0989f16d0589f6897355acda1ee3e9e5402345c7
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2025-03-30 12:34:33 -0600

    Se agregó en LinkTreeView el useState y su setter con valor inicial de social el cual es un arrgelo de objetos que creamos en social.ts dentro de src->data.  en src->types se agregó el type SocialNetwork y DevTreeLink para tipar los props entre componentes como en DevTreeInput. Este es un nuevo componente que se creó para renderizar cada red social que tengamos, hace uso de headless ui con el componente switch, por lo que fue necesaria su instalación con sus types mediante npm install @headlessui/react.

--- src/views/LinkTreeView.tsx
+++ src/views/LinkTreeView.tsx
@@ -1,6 +1,18 @@
+import DevTreeInput from "../components/DevTreeInput";
+import { social } from "../data/social"
+import { useState } from "react"
 
 export default function LinkTreeView() {
+  const [DevTreeLinks, setDevTreeLinks] = useState(social);
+  console.log(DevTreeLinks);
+  
   return (
-    <div>LinkTreeView</div>
+    <div className="space-y-5">
+      {
+        DevTreeLinks.map(item=>(
+          <DevTreeInput key={item.name} item={item} />
+        ))
+      }
+    </div>
   )
 }
