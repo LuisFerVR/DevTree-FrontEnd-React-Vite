7d53d4d7a680396934177ba4d34e961927e3ea10
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2025-04-03 14:42:48 -0600

    Consumiendo endpoint del backend desde el front  en HandleView y DevTreeApi, obteniendo y pasando por parámetro en la url el handle.

--- src/views/HandleView.tsx
+++ src/views/HandleView.tsx
@@ -1,5 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom"
+import { getUserByHandle } from "../api/DevTreeAPI";
 
 export default function HandleView() {
+  const params = useParams();
+  const handle = params.handle!;
+  console.log(handle);
+
+  const {data,isError,isLoading} = useQuery({
+    queryFn: () => getUserByHandle(handle),
+    queryKey: ['handle', handle],
+    retry:1
+  })
+  console.log(data);
+  
   return (
     <div>HandleView</div>
   )
