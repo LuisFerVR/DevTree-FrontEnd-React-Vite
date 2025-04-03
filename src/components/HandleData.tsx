24b31f9addf899e6e1092085852e69246c5580b1
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2025-04-03 16:56:22 -0600

    Se renderizó la información devuelta del backend sobre HandleData.

--- src/components/HandleData.tsx
+++ src/components/HandleData.tsx
@@ -1,15 +1,35 @@
-import { UserHandle } from "../types"
+import { SocialNetwork, UserHandle } from "../types"
 
 type HandleDataProps = {
     data: UserHandle
 }
 
 export default function HandleData({data}: HandleDataProps) {
+
+    const links : SocialNetwork[] = JSON.parse(data.links).filter((link: SocialNetwork) => link.enabled);
+
   return (
     <div className="space-y-6 text-white">
         <p className="text-5xl text-center font-black">{data.handle}</p>
-        {data.image && <img src={data.image} alt="Imagen de perfil" className="mx-auto max-w-[250px]"/>}
+        {data.image && <img src={data.image} alt="Imagen de perfil" className="mx-auto max-w-[150px]"/>}
         <p className="text-lg text-center font-bold">{data.description}</p>
+
+        <div className="flex flex-col mt-20 gap-6">
+            {links.length ?
+            links.map(link => (
+                <a 
+                    key={link.name}
+                    className="bg-white flex justify-start px-5 py-2 items-center gap-5 rounded-lg "
+                    href={link.url}
+                    target="_blank"
+                    rel="noreferrer noopener"
+                >
+                    <img src={`/social/icon_${link.name}.svg`} alt="icono red social" className="max-w-[50px]"></img>
+                    <p className="text-black capitalize font-bold text-lg">Visita mi: {link.name}</p> 
+                </a>
+            ))
+            : <p className="text-center">No hay enlaces en este perfil</p>}
+        </div>
     </div>
   )
 }
